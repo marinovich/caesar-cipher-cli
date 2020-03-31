ca012fc3ebf35b138a80963814a8f8c48bfdc043
Alexander Marinovich <marinovich.a.n@gmail.com>
2020-03-31 10:32:00 +0300

    init: create basic structure

---
+++ index.js
@@ -0,0 +1,33 @@
+const minimist = require('minimist');
+const error = require('./utils/error');
+
+module.exports = () => {
+  const args = minimist(process.argv.slice(2))
+  let command = args._[0] || 'help';
+
+  if (args.version || args.v) {
+    command = 'version';
+  }
+
+  if (args.help || args.h) {
+    command = 'help';
+  }
+
+  switch (command) {
+    case 'today':
+      require('./commands/today')(args)
+      break
+
+    case 'version':
+      require('./commands/version')(args)
+      break
+
+    case 'help':
+      require('./commands/help')(args)
+      break
+
+    default:
+      error(`"${command}" is not a valid command!`, true)
+      break
+  }
+}
